7c0e1ad0983e33cecaf74e6640d68116f2648bfb
Vindhya N <vindhyan@Vindhyas-MacBook-Air.local>
2023-07-11 14:26:33 +0100

    Adding Smart Greenhouse project to Github

---
+++ newmodel.py
@@ -0,0 +1,38 @@
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.models import model_from_yaml
+import numpy as np
+from keras.preprocessing import image
+import os
+
+# load YAML and create model
+yaml_file = open('model1.yaml', 'r')
+loaded_model_yaml = yaml_file.read()
+yaml_file.close()
+loaded_model = model_from_yaml(loaded_model_yaml)
+# load weights into new model
+loaded_model.load_weights("model1.h5")
+print("Loaded model from disk")
+ 
+# evaluate loaded model on test data
+#loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+#score = loaded_model.evaluate(X, Y, verbose=0)
+#print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
+
+test_image = image.load_img('dataset2/single_prediction/disease_or_healthy.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = loaded_model.predict(test_image)
+
+#training_set.class_indices
+if result[0][0] == 1:
+	prediction = 'healthy'
+elif result[0][0] == 0:
+	prediction = 'disease'
+else:
+	prediction = 'random'
+print(prediction)
